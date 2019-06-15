85061378bcade2214aff14a0fd886a487d9c5ac4
Garrison Shepard <shepard.garrisont@gmail.com>
2019-06-15 11:26:16 -0400

    data model complete

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ const app = express();
 const apiRouter = ('./EndPoint')
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
-
+const {User, Post, Comment} = require('./DataModel/index.js');
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 
