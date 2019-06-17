34a36b305b28325ddd20190c5fd405701f114b05
Garrison Shepard <shepard.garrisont@gmail.com>
2019-06-16 23:49:05 -0400

    comment routes

--- api/comment.js
+++ api/comment.js
@@ -5,6 +5,7 @@ const {User, Post, Comment} = require('../data_model/index');
 router.get('/', (req, res, next) => {
     Comment.findAll().then((comment) => res.status(201).json(comment));
 });
+
 //get all comments for a specific post
 router.get('/post/:id', (req, res, next) => {
   Comment.findAll({where:{postId:[req.params.id]}}).then((comment) => res.status(201).json(comment));
@@ -20,7 +21,16 @@ router.post('/', (req, res, next) => {
     Comment.create(req.body).then((comment) => res.status(201).json(comment));
 });
 
+//update a comment (remember all attirbutes must be included for put to work )
+router.put('/:id', (req, res, next) => {
+    Comment.findByPk(req.params.id).then((comment) => comment.update(req.body)).then((updatedComment) => res.status(201).json(updatedComment));
+});
 
-
+//delete a comment
+router.delete('/:id', (req, res, next) => {
+    Comment.destroy({
+      where:{id: req.params.id}
+    }).then((comment) => res.status(201).json(comment));
+});
 
 module.exports = router;
