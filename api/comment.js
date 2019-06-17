9df0c38cf27778a2a94d963620a3c3f2349f86ec
Garrison Shepard <shepard.garrisont@gmail.com>
2019-06-16 23:39:36 -0400

    add GET methods for comments

--- api/comment.js
+++ api/comment.js
@@ -1,17 +1,23 @@
 const router = require('express').Router();
 const {User, Post, Comment} = require('../data_model/index');
 
-//get all comments for a post
-router.get('/post/id:', (req, res, next) => {
-    Comment.findAll({
-        where:{postId: req.params.id}
-    }).then((comment) => res.status(201).json(comment));
+//get all comments
+router.get('/', (req, res, next) => {
+    Comment.findAll().then((comment) => res.status(201).json(comment));
+});
+//get all comments for a specific post
+router.get('/post/:id', (req, res, next) => {
+  Comment.findAll({where:{postId:[req.params.id]}}).then((comment) => res.status(201).json(comment));
 });
 
+//get all comments for a specific user
+router.get('/user/:id', (req, res, next) => {
+  Comment.findAll({where:{userId:[req.params.id]}}).then((comment) => res.status(201).json(comment));
+});
 
-
+//create a comment
 router.post('/', (req, res, next) => {
-    Comment.create()
+    Comment.create(req.body).then((comment) => res.status(201).json(comment));
 });
 
 
