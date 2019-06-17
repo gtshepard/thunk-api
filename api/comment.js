2e6488151f29f920e5005b552fd1ec988e4bb582
Garrison Shepard <shepard.garrisont@gmail.com>
2019-06-16 23:23:28 -0400

    add comment model

--- api/comment.js
+++ api/comment.js
@@ -1,4 +1,20 @@
 const router = require('express').Router();
 const {User, Post, Comment} = require('../data_model/index');
 
+//get all comments for a post
+router.get('/post/id:', (req, res, next) => {
+    Comment.findAll({
+        where:{postId: req.params.id}
+    }).then((comment) => res.status(201).json(comment));
+});
+
+
+
+router.post('/', (req, res, next) => {
+    Comment.create()
+});
+
+
+
+
 module.exports = router;
