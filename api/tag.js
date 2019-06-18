b0d68db6de562a6bc78481259c2ce8e55fadd70c
Garrison Shepard <shepard.garrisont@gmail.com>
2019-06-18 11:56:29 -0400

    create seed for database

--- api/tag.js
+++ api/tag.js
@@ -4,18 +4,25 @@ const {User, Post, Comment, Tag} = require('../data_model/index');
 //get all hashtags for a post
 
 router.post('/post/:id', (req, res, next) => {
-  Tag.create(req.body).then((tag) => res.status(201).json(tag));
-  //Post.findByPk(req.params.id).then((post) => .then((post, tag) => post.addTags([tag])).then((tag) => res.status(201).json(tag));
-  //Post.findByPk(req.params.id).then(() => addTag({tag})).then((tag) => post.addTags(tag)).then((tag) => res.status(201).json(tag))
-  //const post = Post.findByPk(req.params.id);
-  //Tag.create({tag:req.body}).then(function(tag) {post.addTags([tag])});
-  //post.addTags([tag])
-//  const t = Tag.create({tag: req.body});
-  //Post.findByPk(req.params.id).then((post) => {
-  //}).then((post) => res.status.json(post));
+  console.log(req.body);
 
+  Post.findByPk(req.params.id).then((post) => {
+    console.log(post);
+  })
 });
 
+
+
+//Tag.create(req.body).then((tag) => res.status(201).json(tag));
+
+//Post.findByPk(req.params.id).then((post) => .then((post, tag) => post.addTags([tag])).then((tag) => res.status(201).json(tag));
+//Post.findByPk(req.params.id).then(() => addTag({tag})).then((tag) => post.addTags(tag)).then((tag) => res.status(201).json(tag))
+//const post = Post.findByPk(req.params.id);
+//Tag.create({tag:req.body}).then(function(tag) {post.addTags([tag])});
+//post.addTags([tag])
+//  const t = Tag.create({tag: req.body});
+//Post.findByPk(req.params.id).then((post) => {
+//}).then((post) => res.status.json(post));
 
 //Well, I solved the problem with setCoins, above. Apparently it takes id numbers and not objects, so this works:
 /*Ledger.findById(22).then(ledger=>{
