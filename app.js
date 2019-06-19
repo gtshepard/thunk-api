4ffa974aff72e6f3b474cfdeb6e6b4e05131d34b
Garrison Shepard <shepard.garrisont@gmail.com>
2019-06-19 12:25:47 -0400

    allow CORS

--- app.js
+++ app.js
@@ -6,15 +6,30 @@ const morgan = require('morgan');
 const session = require('express-session');
 const passport = require('passport');
 const PORT = process.env.PORT
-
 require('dotenv').config();
+
+let allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+      res.send(200);
+    }
+    else {
+      next();
+    }
+};
+
+
 //logging middleware
 app.use(morgan('dev'));
-
 //body parsing middleware
 app.use(bodyParser.json());
 app.use(express.urlencoded({extended: true}));
-
+//allow cross domain
+app.use(allowCrossDomain);
 app.use(session({
   secret: process.env.SECRET,
   resave: false,
