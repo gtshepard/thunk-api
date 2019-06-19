fcca2b6f09c8ab256aa3d3086a21129c0a3c5102
Garrison Shepard <shepard.garrisont@gmail.com>
2019-06-19 11:04:58 -0400

    remove down and up from post attributes

--- data_model/PostModel.js
+++ data_model/PostModel.js
@@ -1,8 +1,6 @@
 module.exports = (dataType, db) => {
    return db.define('post', {
       text: dataType.STRING,
-      downVote: dataType.INTEGER,
-      upVote: dataType.INTEGER,
       lattitude: dataType.DOUBLE,
       longitude: dataType.DOUBLE,
       report: dataType.STRING
